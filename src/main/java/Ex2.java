187525b27147700342517f1a4285162352378cc7
G3tt1ngBeTTer <fts_korin@bk.ru>
2022-02-09 09:58:27 +0300

    Simple Console Calculator and Finder Max String in Array

---
+++ src/main/java/Ex2.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+/**
+ * Created by Danila Shestipalov
+ *
+ * @author G3B
+ * @see #main(String[])
+ */
+
+public class Ex2 {
+
+    public static void main(String[] args){
+        /**Объявляем переменную для удобства выбора проекта*/
+        int chooseProject=0;
+        try{
+            /**Производим общение с пользователем*/
+            Scanner console = new Scanner(System.in);
+            System.out.println("Выберите номер необходимой операции:");
+            System.out.println("1 - Простой консольный калькулятор");
+            System.out.println("2 - Поиск самого длинного слова среди введенных");
+            System.out.print("Ввведите нужный номер операции: ");
+            chooseProject=console.nextInt();
+            /**По номеру операции запускаем нужную*/
+            switch (chooseProject){
+                case 1:
+                    Ex1.main(null);
+                    break;
+                case 2:
+                    maxStringInArray.main(null);
+                    break;
+                default:
+            /***/
+                System.out.println("Введено неправельное значение, повторение операции");
+                System.out.println();
+                Ex2.main(null);
+            }
+        }
+        catch (Exception err){
+            System.err.println(err.getStackTrace().toString());
+        }
+    }
+}
---
+++ src/main/java/maxStringInArray.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+/**
+ * Created by Danila Shestipalov
+ *
+ * @author G3B
+ * @see #main(String[])
+ */
+
+public class maxStringInArray {
+    public static void main (String [] args){
+        System.out.println();
+        int arrayCount=0;
+        String maxWord="";
+        int maxLength=0;
+        System.out.print("Введите количество слов в массиве: ");
+        Scanner scanner = new Scanner(System.in);
+        arrayCount = scanner.nextInt();
+        String [] stringArray = new String[arrayCount];
+        System.out.println("Вводите массив по одному слову, после каждого слова нажимайте enter");
+        scanner.nextLine();
+        scanner = new Scanner(System.in);
+        for (int i=0;i<stringArray.length; i++) {
+            stringArray[i]= scanner.nextLine();
+            if (maxLength < stringArray[i].length()) {
+                maxWord=stringArray[i];
+                maxLength=stringArray[i].length();
+            }
+        }
+
+        System.out.println("Максимальное слово по длине - "+ maxWord);
+    }
+}
