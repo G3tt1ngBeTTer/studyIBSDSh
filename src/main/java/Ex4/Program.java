3c435a325c7d339a7bc52e0466385a5a8f50611c
G3tt1ngBeTTer <fts_korin@bk.ru>
2022-02-14 09:56:53 +0300

    Ex4 OOP Calculator After bug fixes

--- src/main/java/Ex4/Program.java
+++ src/main/java/Ex4/Program.java
@@ -12,38 +12,9 @@ import java.util.Scanner;
 public class Program {
 
     public static void main(String[] args) {
-        /**Получаем данные от пользователя*/
-        Scanner scanner=new Scanner(System.in);
-        String curOp;
-        float firstAgr; float secondAgr;
-        System.out.println("Выберите знак операции: ");
-        System.out.println(" '+' - Сложение; '-' - Вычитание; '*' - Умножение; '/' -  Деление");
-        System.out.print("Введите знак вашей операции: ");
-        curOp = scanner.nextLine();
-        System.out.print("Введите первое число: ");
-        firstAgr = scanner.nextFloat();
-        System.out.print("Введите второе число: ");
-        secondAgr = scanner.nextFloat();
-        scanner.close();
-        Arguments arguments = new Arguments(firstAgr, secondAgr);
-        /**Относительно операции получаем и выводим результат*/
-        switch (curOp) {
-            case "+":
-                System.out.println("Результат = " + Add.getResult());
-            break;
-            case "-":
-                System.out.println("Результат = " + Sub.getResult());
-            break;
-            case "*":
-                System.out.println("Результат = " + Mul.getResult());
-            break;
-            case "/":
-                System.out.println("Результат = " + Div.getResult());
-            break;
-            default:
-                System.out.println("Ошибка в выборе операции, повторите воод примера");
-                Program.main(null);
-        }
-
+        Calculator calc1 = new Calculator(28, 32, "+");
+        System.out.println(calc1.getResult());
+        Calculator calc2 = new Calculator(28, 32, "fafaasf");
+        System.out.println(calc2.getResult());
     }
 }
