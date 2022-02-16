0af5e579e2213e801884e71f6bc0fc69ee99845b
G3tt1ngBeTTer <fts_korin@bk.ru>
2022-02-16 09:15:57 +0300

    Ex4 OOP Calculator After bug fixed again

--- src/main/java/Ex4/Program.java
+++ src/main/java/Ex4/Program.java
@@ -1,5 +1,7 @@
 package Ex4;
 
+import java.util.Scanner;
+
 /**
  * Created by Danila Shestipalov
  *
@@ -12,12 +14,31 @@ package Ex4;
 public class Program {
 
     public static void main(String[] args) {
-        /**Создаём новые объекты и выводим из них результат в консоль методом getResult*/
-        Calculator calc1 = new Calculator(28, 0, "/");
-        System.out.println("Результат вычисления 1 = " + calc1.getResult());
-        Calculator calc2 = new Calculator(28, 32, "fafaasf");
-        System.out.println("Результат вычисления 2 = " +calc2.getResult());
-        Calculator calc3 = new Calculator(6, -2, "-");
-        System.out.println("Результат вычисления 3 = " +calc3.getResult());
+//        /**Создаём новые объекты и выводим из них результат в консоль методом getResult*/
+//        Calculator calc1 = new Calculator(28, 0, "/");
+//        System.out.println("Результат вычисления 1 = " + calc1.getResult());
+//        Calculator calc2 = new Calculator(28, 32, "fafaasf");
+//        System.out.println("Результат вычисления 2 = " +calc2.getResult());
+//        Calculator calc3 = new Calculator(6, -2, "-");
+//        System.out.println("Результат вычисления 3 = " +calc3.getResult());
+//
+        /**Выводим информацию для пользователя*/
+        String operation;
+        int firstAgr;
+        int secondAgr;
+        System.out.println("Введите знак операции, из предложенных:");
+        System.out.println("1)Сложение + 2)Вычитание - 3)Умножение * 4)Деление /");
+
+        /**Получаем данные от пользователя и записываем в переменные*/
+        System.out.print("Выш выбор: ");
+        Scanner scanner = new Scanner(System.in);
+        operation = scanner.nextLine();
+        System.out.print("Введите первое число: ");
+        firstAgr = scanner.nextInt();
+        System.out.print("Введите второе число: ");
+        secondAgr = scanner.nextInt();
+        scanner.close();
+        Calculator calc4 = new Calculator(firstAgr,secondAgr,operation);
+        System.out.println("Результат вычисления = " +calc4.getResult());
     }
 }
