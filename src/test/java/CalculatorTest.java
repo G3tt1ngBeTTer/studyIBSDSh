8db359e6898c9d881c310f2af662f1494118c71b
G3tt1ngBeTTer <fts_korin@bk.ru>
2022-02-18 09:59:16 +0300

    Ex7 Exceptions JUnit After fixed

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -7,36 +7,33 @@ import Ex4.Calculator;
 public class CalculatorTest {
 
     /**
-     * В данных тестах результат ноль, вызывающий ошибку правилен и тест направлен на отслеживание обнаружения этой ошибки
-     * @see #getResultTestWithBadOperation()
-     * @see #getResultTestWithNullSecondArg()
-     * */
-    @Test
-    public void getResultTestWithBadOperation () {
-        Calculator calc = new Calculator(4,3,"оо");
-        Assert.assertEquals("Неправильная операция", 0, calc.getResult(),0);
-    }
-
-    @Test
-    public void getResultTestWithNullSecondArg() {
-        Calculator calc = new Calculator(4,0,"/");
-        Assert.assertEquals("На ноль делить нельзя", 0, calc.getResult(),0);
-    }
-
-    /**
-     * Данный тест производит проверку всех текущих действий в ООП калькуляторе
-     * @see #getResultTestWithGoodArguments()
+     * Данные тесты производят проверку всех текущих действий в ООП калькуляторе
+     * @see #getResultTestWithGoodArgumentsAdd()
+     * @see #getResultTestWithGoodArgumentsSub()
+     * @see #getResultTestWithGoodArgumentsMul()
+     * @see #getResultTestWithGoodArgumentsDiv()
      * */
 
     @Test
-    public void getResultTestWithGoodArguments(){
+    public void getResultTestWithGoodArgumentsDiv() throws Calculator.nullDivException, Calculator.operationException {
         Calculator calc = new Calculator(4,2,"/");
         Assert.assertEquals("Неправильный результат при деление 4 на 2", 2, calc.getResult(),0);
-        Calculator calc1 = new Calculator(6,-2,"-");
-        Assert.assertEquals("Неправильный результат при вычитании 6-(-2)", 8, calc1.getResult(),0);
-        Calculator calc2 = new Calculator(6,4,"+");
-        Assert.assertEquals("Неправильный результат при сложение 6+4)", 10, calc2.getResult(),0);
-        Calculator calc3 = new Calculator(8,4,"*");
-        Assert.assertEquals("Неправильный результат при умножение 8*4", 32, calc3.getResult(),0);
+    }
+    @Test
+    public void getResultTestWithGoodArgumentsSub() throws Calculator.nullDivException, Calculator.operationException {
+        Calculator calc = new Calculator(6,-2,"-");
+        Assert.assertEquals("Неправильный результат при вычитании 6-(-2)", 8, calc.getResult(),0);
+    }
+
+    @Test
+    public void getResultTestWithGoodArgumentsAdd() throws Calculator.nullDivException, Calculator.operationException {
+        Calculator calc = new Calculator(6,4,"+");
+        Assert.assertEquals("Неправильный результат при сложение 6+4)", 10, calc.getResult(),0);
+    }
+
+    @Test
+    public void getResultTestWithGoodArgumentsMul() throws Calculator.nullDivException, Calculator.operationException {
+        Calculator calc = new Calculator(8,4,"*");
+        Assert.assertEquals("Неправильный результат при умножение 8*4", 32, calc.getResult(),0);
     }
 }
